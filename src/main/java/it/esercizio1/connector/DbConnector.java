291d185bf9de66faf9aa73423d0abb07ee30c2c5
DanieleMaglio <danielemaglio98@gmail.com>
2019-09-13 13:13:09 +0200

    ciao

--- src/main/java/it/esercizio1/connector/DbConnector.java
+++ src/main/java/it/esercizio1/connector/DbConnector.java
@@ -5,10 +5,56 @@
  */
 package it.esercizio1.connector;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  *
  * @author Utente
  */
 public class DbConnector {
+    private static final String URL="jdbc:mysql://localhost:3306/world";
+    private static final String USER="admin";
+    private static final String PASSWORD="admin";
+    private final static String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String SELECT_CONTINENTS="select Continent from country;";
     
+    public static Connection connetti() throws SQLException{
+        try{
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DbConnector.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Connection conn=DriverManager.getConnection(URL,USER,PASSWORD);
+        return conn;
+    }
+    
+    public static List<String> getContinenti(){
+        List<String> continenti=null;
+   
+        try(Connection conn=connetti();
+                PreparedStatement pstmt=conn.prepareStatement(SELECT_CONTINENTS);){
+            ResultSet rs= pstmt.executeQuery();
+            while(rs.next()){
+                if(continenti==null){
+                    continenti=new ArrayList();
+                    continenti.add(rs.getString("Continent"));
+                }
+                if((continenti.contains(rs.getString("Continent")))==false){
+                    continenti.add(rs.getString("Continent"));
+                }
+            }
+        }   catch (SQLException ex) {
+            Logger.getLogger(DbConnector.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return continenti;
+    }
 }
--- src/main/java/it/esercizio1/controller/InerimentoController.java
+++ src/main/java/it/esercizio1/controller/InerimentoController.java
@@ -5,11 +5,27 @@
  */
 package it.esercizio1.controller;
 
+import it.esercizio1.connector.DbConnector;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  *
  * @author Utente
  */
-//@Controller
+@Controller
 public class InerimentoController {
+    
+    List<String> continenti;
+    
+    @GetMapping("/")
+    public String main(Model model){
+        continenti=DbConnector.getContinenti();
+        model.addAttribute("continenti", continenti);
+        
+        return "continente";
+    }
     
 }
