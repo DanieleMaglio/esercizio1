d59f3a542823d4f02350cc93011fd249b976b7c1
DanieleMaglio <danielemaglio98@gmail.com>
2019-09-13 12:55:40 +0200

    ciao

---
+++ src/main/java/it/esercizio1/controller/InerimentoController.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.esercizio1.controller;
+
+/**
+ *
+ * @author Utente
+ */
+//@Controller
+public class InerimentoController {
+    
+}
